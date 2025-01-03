9f2f125779fde7c4db3434a5d99aa0ef1a554b09
Sadman-Sakib748 <sadman.sakib34523@gmail.com>
2025-01-04 00:02:52 +0600

    second comment

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    node:true,
+    node: true,
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 
 const Home = () => {
     return (
         <div>
-            <h2>Home Page</h2>
+            <h2 className='text-3xl bg-stone-700 '>Home Page</h2>
+           
         </div>
     );
 };
